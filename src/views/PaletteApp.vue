545bc1769cd2f447fe0e6b22e96a400bafbba10c
bear000660 <mnstapazuru661@gmail.com>
2022-08-22 22:14:50 +0900

    変化させるRGBAの要素を選べるようにしました。

--- src/views/PaletteApp.vue
+++ src/views/PaletteApp.vue
@@ -2,11 +2,23 @@
   <h1>Vue パレット</h1>
   <div class="app">
     <div class="palette_area">
+      <!-- 下からY軸を表す部分 -->
       <div class="palette_Yaxis palette_axis">
         <div class="palette_area_axis_index">Low</div>
-        <div class="palette_area_axis_element">Green</div>
+        <div class="palette_area_axis_element">
+          <select v-model="selectedX" class="palette_area_axis_element">
+            <option
+              v-for="(color, index) in colorElementsList"
+              v-bind:value="color"
+              v-bind:key="index"
+            >
+              {{ color.text }}
+            </option>
+          </select>
+        </div>
         <div class="palette_area_axis_index">High</div>
       </div>
+      <!-- Y軸を表す部分終わり -->
       <!-- 下のdivタグが色を表示 -->
       <div
         class="palette"
@@ -17,14 +29,26 @@
         v-on:mousemove="changeColor"
       ></div>
       <!-- 色を表示する部分ここまで -->
+      <!-- 下からX軸を表す部分 -->
       <div class="palette_axis palette_Xaxis">
         <div class="palette_area_axis_index">Low</div>
-        <dic class="palette_area_axis_element">Red</dic>
+        <dic class="palette_area_axis_element"
+          ><select v-model="selectedY" class="palette_area_axis_element">
+            <option
+              v-for="(color, index) in colorElementsList"
+              v-bind:value="color"
+              v-bind:key="index"
+            >
+              {{ color.text }}
+            </option>
+          </select></dic
+        >
         <div class="palette_area_axis_index">High</div>
       </div>
+      <!-- X軸を表す部分終わり -->
     </div>
     <!-- 下のpタグが現在の色の説明 -->
-    <p>rgba( {{ red }}, {{ green }}, 200, 0.5 )</p>
+    <p>rgba( {{ red }}, {{ green }}, {{ blue }}, {{ alpha }} )</p>
     <div class="colors-container">
       <div class="mini-palette"></div>
     </div>
@@ -35,16 +59,50 @@
 export default {
   data() {
     return {
+      Xpointer: 0,
+      Ypointer: 0,
       red: 0,
       green: 0,
       blue: 200,
       alpha: 0.5,
+      colorElementsList: [
+        { text: "Red", value: [this.red] },
+        { text: "Green", value: [this.green] },
+        { text: "Blue", value: [this.blue] },
+        { text: "Alpha", value: [this.alpha] },
+      ],
+      selectedX: { text: "Green", value: [this.green] },
+      selectedY: { text: "Red", value: [this.red] },
     }
   },
   methods: {
     changeColor(event) {
-      this.red = event.offsetX
-      this.green = event.offsetY
+      this.Xpointer = event.offsetX
+      this.Ypointer = event.offsetY
+      if (this.selectedX.text === "Red") {
+        this.red = this.Xpointer
+      }
+      if (this.selectedX.text === "Green") {
+        this.green = this.Xpointer
+      }
+      if (this.selectedX.text === "Blue") {
+        this.blue = this.Xpointer
+      }
+      if (this.selectedX.text === "Alpha") {
+        this.alpha = this.Xpointer
+      }
+      if (this.selectedY.text === "Red") {
+        this.red = this.Ypointer
+      }
+      if (this.selectedY.text === "Green") {
+        this.green = this.Ypointer
+      }
+      if (this.selectedY.text === "Blue") {
+        this.blue = this.Ypointer
+      }
+      if (this.selectedY.text === "Alpha") {
+        this.alpha = this.Ypointer
+      }
     },
   },
 }
@@ -62,7 +120,7 @@ export default {
   height: 255px;
 }
 .palette_area {
-  width: 375px;
+  width: 425px;
   display: flex;
   flex-wrap: wrap;
 }
