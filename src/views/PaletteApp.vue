9737adda1b05cf1a180683afd998cf7dc3fc11d3
bear000660 <mnstapazuru661@gmail.com>
2022-08-23 16:53:17 +0900

    色を保存する機能、パレットをクリックした際にクリックした位置を示す機能を追加しました。

--- src/views/PaletteApp.vue
+++ src/views/PaletteApp.vue
@@ -8,9 +8,9 @@
     </p>
     <!-- 説明終わり -->
     <!-- 以下からメインパレットの大枠 -->
-    <h3 class="palette_title">メインパレット</h3>
-    <!-- クリックの状態を表示 -->
-    <div class="clickStatus" v-on:click="decideColor">
+    <h3 class="palette_title title">メインパレット</h3>
+    <!-- クリックの状態を表示。下のdivタグ内の関数decideColorの引数はパレット外でクリックされることを表します。 -->
+    <div class="clickStatus" v-on:click="decideColor(false)">
       色の変化：
       <div v-if="decideTrigger">停止中</div>
       <div v-else>可能</div>
@@ -21,7 +21,7 @@
       <div class="palette_Yaxis palette_axis">
         <div class="palette_area_axis_index">Low</div>
         <div class="palette_area_axis_element">
-          <select v-model="selectedX" class="palette_area_axis_element">
+          <select v-model="selectedY" class="palette_area_axis_element">
             <option
               v-for="(color, index) in colorElementsList"
               v-bind:value="color"
@@ -34,7 +34,7 @@
         <div class="palette_area_axis_index">High</div>
       </div>
       <!-- Y軸を表す部分終わり -->
-      <!-- 下のdivタグが色を表示 -->
+      <!-- 下のdivタグが色を表示。divタグ内にある関数decideColorの引数はpalette上でクリックしていることを表します。 -->
       <div
         class="palette"
         v-bind:style="{
@@ -42,14 +42,25 @@
             'rgba(' + red + ', ' + green + ', ' + blue + ', ' + alpha + ')',
         }"
         v-on:mousemove="changeColor"
-        v-on:click="decideColor"
-      ></div>
+        v-on:click="decideColor(true)"
+      >
+        <!-- 下のdivタグはクリックしたところをさすポインター -->
+        <div
+          v-show="decideTrigger && isFocusPalette"
+          class="palette_pointer"
+          v-bind:style="{
+            left: pointerPositionX - 5 + 'px',
+            top: pointerPositionY - 5 + 'px',
+          }"
+        ></div>
+        <!-- ポインター終わり -->
+      </div>
       <!-- 色を表示する部分ここまで -->
       <!-- 下からX軸を表す部分 -->
       <div class="palette_axis palette_Xaxis">
         <div class="palette_area_axis_index">Low</div>
         <dic class="palette_area_axis_element"
-          ><select v-model="selectedY" class="palette_area_axis_element">
+          ><select v-model="selectedX" class="palette_area_axis_element">
             <option
               v-for="(color, index) in colorElementsList"
               v-bind:value="color"
@@ -63,18 +74,76 @@
       </div>
       <!-- X軸を表す部分終わり -->
     </div>
-    <!-- 下のpタグが現在の色の説明 -->
-    <p>
+    <!-- 下のpタグが現在の色のRGBA値を表す -->
+    <p v-show="!decideTrigger">
       rgba(
-      <input class="inputColor" v-model="red" v-bind:placeholder="red" /> ,
-      <input class="inputColor" v-model="green" v-bind:placeholder="green" />,
-      <input class="inputColor" v-model="blue" v-bind:placeholder="green" />,
-      <input class="inputColor" v-model="alpha" v-bind:placeholder="alpha" /> )
+      {{ red }}, {{ green }}, {{ blue }}, {{ alpha }} )
     </p>
-    <!-- 現在の色の説明終わり -->
+
+    <p v-show="decideTrigger">
+      rgba(
+      <input
+        class="inputColor"
+        v-model="red"
+        v-bind:placeholder="red"
+        v-on:click="focusInput"
+      />
+      ,
+      <input
+        class="inputColor"
+        v-model="green"
+        v-bind:placeholder="green"
+        v-on:click="focusInput"
+      />,
+      <input
+        class="inputColor"
+        v-model="blue"
+        v-bind:placeholder="green"
+        v-on:click="focusInput"
+      />,
+      <input
+        class="inputColor"
+        v-model="alpha"
+        v-bind:placeholder="alpha"
+        v-on:click="focusInput"
+      />
+      )
+    </p>
+    <!-- 現在の色を表す部分終わり -->
+    <!-- 保存ボタン -->
+    <button class="saveButton" v-on:click="saveColor">保存</button>
+    <!-- 保存ボタン終わり -->
+    <!-- 以下から保存した色を表示する部分 -->
+    <h3 class="colors-container_title title">保存した色</h3>
     <div class="colors-container">
-      <div class="mini-palette"></div>
+      <div v-if="savedColors.length === 0" class="displayNothing">Nothing</div>
+      <div
+        v-else
+        v-for="(savedColor, index) in savedColors"
+        v-bind:key="index"
+        class="mini-palette_area"
+      >
+        <div
+          class="mini-palette"
+          v-bind:style="{
+            'background-color':
+              'rgba(' +
+              savedColor.red +
+              ',' +
+              savedColor.green +
+              ',' +
+              savedColor.blue +
+              ',' +
+              savedColor.alpha +
+              ')',
+          }"
+        ></div>
+        <button class="deleteColorButton" v-on:click="deleteColor(index)">
+          削除
+        </button>
+      </div>
     </div>
+    <!-- 保存した色を表示する部分終わり -->
   </div>
 </template>
 
@@ -85,18 +154,23 @@ export default {
       decideTrigger: false,
       Xpointer: 0,
       Ypointer: 0,
+      pointerPositionX: this.Xpointer,
+      pointerPositionY: this.Ypointer,
       red: 0,
       green: 0,
       blue: 200,
       alpha: 0.5,
       colorElementsList: [
-        { text: "Red", value: [this.red] },
-        { text: "Green", value: [this.green] },
-        { text: "Blue", value: [this.blue] },
-        { text: "Alpha", value: [this.alpha] },
+        { text: "None" },
+        { text: "Red" },
+        { text: "Green" },
+        { text: "Blue" },
+        { text: "Alpha" },
       ],
-      selectedX: { text: "Green", value: [this.green] },
-      selectedY: { text: "Red", value: [this.red] },
+      selectedX: { text: "Green" },
+      selectedY: { text: "Red" },
+      savedColors: [],
+      isFocusPalette: true,
     }
   },
   methods: {
@@ -115,7 +189,7 @@ export default {
           this.blue = this.Xpointer
         }
         if (this.selectedX.text === "Alpha") {
-          this.alpha = this.Xpointer
+          this.alpha = this.Xpointer / 255
         }
         if (this.selectedY.text === "Red") {
           this.red = this.Ypointer
@@ -127,17 +201,36 @@ export default {
           this.blue = this.Ypointer
         }
         if (this.selectedY.text === "Alpha") {
-          this.alpha = this.Ypointer
+          this.alpha = this.Ypointer / 255
         }
       }
     },
     // 以下はクリックのたびに変数名"decideColor"のboolean値を変える関数
-    decideColor() {
+    decideColor(clickInPalette) {
+      this.isFocusPalette = true
       if (this.decideTrigger) {
         this.decideTrigger = false
       } else {
+        if (clickInPalette) {
+          this.pointerPositionX = this.Xpointer
+          this.pointerPositionY = this.Ypointer
+        }
         this.decideTrigger = true
       }
+    },
+    saveColor() {
+      const theColor = { red: "", green: "", blue: "", alpha: "" }
+      theColor.red = this.red
+      theColor.green = this.green
+      theColor.blue = this.blue
+      theColor.alpha = this.alpha
+      this.savedColors.push(theColor)
+    },
+    deleteColor(index) {
+      this.savedColors.splice(index, 1)
+    },
+    focusInput() {
+      this.isFocusPalette = false
     },
   },
 }
@@ -185,11 +278,39 @@ export default {
 .mini-palette {
   min-width: 60px;
   height: 60px;
+  border-radius: 30px;
+}
+.palette_pointer {
+  background-color: white;
+  position: relative;
+  width: 10px;
+  height: 10px;
+  border: solid 2px;
+  border-radius: 10px;
+}
+.deleteColorButton {
+  width: 60px;
+}
+.colors-container_title {
+  margin-top: 30px;
+}
+.displayNothing {
+  margin-left: auto;
+  margin-right: auto;
 }
 .colors-container {
   display: flex;
   flex-wrap: wrap;
-  width: 300px;
+  width: 350px;
   padding-top: 8px;
+  padding-bottom: 8px;
+  border: 5px solid;
+}
+.mini-palette_area {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 10px;
+  margin-left: 10px;
 }
 </style>
