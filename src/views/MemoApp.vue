f7f96d3df7f34e202c4eab56ae53694e4191398e
Futa Ando <futa990628@gmail.com>
2022-08-26 10:07:09 +0900

    一覧のｈ表示順番を逆にした

--- src/views/MemoApp.vue
+++ src/views/MemoApp.vue
@@ -44,7 +44,7 @@
       />
     </div>
     <h4>メモ一覧</h4>
-    <div v-for="(memo, index) in memos" v-bind:key="index">
+    <div v-for="(memo, index) in reverseMemos" v-bind:key="index">
       <div v-bind:style="{ backgroundColor: memo.bgColor }" class="each__memo">
         <div>
           内容： {{ memo.content }} <br />
@@ -83,6 +83,12 @@ export default {
         { id: "blue2", bgc: "#8EB8FF" },
       ],
     }
+  },
+  computed: {
+    // 配列の要素順番を逆順にする
+    reverseMemos() {
+      return this.memos.slice().reverse()
+    },
   },
   methods: {
     addMemo: function () {
